94345129d12d8b9b921f1bd0b2e7b3a03a099468
RamonaH <hader.ramona@gmail.com>
2016-06-11 13:21:12 +0300

    add CRUD

--- js/agenda.js
+++ js/agenda.js
@@ -6,7 +6,13 @@ console.debug('1) loading agenda');
 function getRow(person){
     var firstName= person.firstName;
     var lastName= person.lastName;
-    var row= '<tr><td>'+ firstName +'</td><td>' + lastName +'</td><td>0752094424</td></tr>';
+    var phone= person.phone;
+    var row= '<tr>' +
+        '<td>'+ firstName + '</td>' +
+        '<td>' + lastName + '</td>' +
+        '<td>'+ phone+'</td>'+
+        '<td><button data-id="'+ person.id + '">remove</button></td>'+
+        '</tr>';
     return row;
 }
 
@@ -17,11 +23,32 @@ $.ajax({
     showContacts(contacts);
 });
 
+function removeContact(id){
+
+
+    $.ajax({
+        url: "js/mocks/remove-contact.json",
+        type: 'POST',
+        data: {
+            id: id
+        }
+    }).done(function (contacts) {
+        showContacts(contacts);
+    });
+}
+
 console.debug(' 2) after ajax');
 
 function showContacts (contacts){
+    $('#agenda tbody').html('');
     for (var i = 0; i<contacts.length; i++){
         var person= contacts[i];
         $('#agenda tbody').append(getRow(person));
     }
 }
+
+$('#agenda ').on('click','button',function(){
+    var id = $(this).data('id');
+    console.info ('remove this contact',this, id);
+    removeContact(id);
+});
