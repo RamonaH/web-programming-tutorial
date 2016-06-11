b3ab87ca858bb269870e4d3c7ea00b8f8faedef6
RamonaH <hader.ramona@gmail.com>
2016-06-11 10:46:36 +0300

    agenda

---
+++ js/agenda.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Maxi on 6/11/2016.
+ */
+console.debug('loading agenda');
+var row= '<tr><td>Popescu</td><td>Ionel</td><td>0752094424</td></tr>';
+
+$('#agenda tbody').append(row);
